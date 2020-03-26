cb5153d40bd730508b297a2abedf57c78e449f36
na.savchenko <na.savchenko@tinkoff.ru>
2020-03-26 17:18:38 +0700

    Commit initial ecosystem

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GrpcMobileDemo' do
+  use_frameworks!
+
+  # Pods for GrpcMobileDemo
+  pod 'GRPC-Client', path: './grpc-client'
+
+  target 'GrpcMobileDemoTests' do
+    inherit! :search_paths
+  end
+
+  target 'GrpcMobileDemoUITests' do
+    inherit! :search_paths
+  end
+
+end
---
+++ grpc-ios-berryTests/grpc_ios_berryTests.swift
@@ -0,0 +1,34 @@
+//
+//  grpc_ios_berryTests.swift
+//  grpc-ios-berryTests
+//
+//  Created by na.savchenko on 26.03.2020.
+//  Copyright © 2020 Mel Absinthiatum. All rights reserved.
+//
+
+import XCTest
+@testable import grpc_ios_berry
+
+class grpc_ios_berryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
